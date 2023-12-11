dcac349f18c233bc7e467cefa9867f26e8110b96
Mike Thomas <mikepthomas@outlook.com>
2023-12-11 23:11:05 +0000

    add remark plugins

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,9 @@
 import { remark } from 'remark'
+import emoji from 'remark-emoji'
+import gfm from 'remark-gfm'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(emoji).use(gfm).use(html).process(markdown)
   return result.toString()
 }
