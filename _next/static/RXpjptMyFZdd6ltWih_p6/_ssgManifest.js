5cf826d6cde09edcc74decf1039fe54e5a9d6947
Mike Thomas <mikepthomas@outlook.com>
2025-02-21 22:44:50 +0000

    Deploy Site to GitHub Pages

---
+++ _next/static/RXpjptMyFZdd6ltWih_p6/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Ffahservices\u002F[slug]","\u002Ffahview\u002F[slug]","\u002Ffahweb\u002F[slug]","\u002Fjfold\u002F[slug]","\u002Fprojects","\u002Fprojects\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
