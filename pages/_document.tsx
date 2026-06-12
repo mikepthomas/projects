675dee938dddc9ba67189231f2691d7390df8e1a
Mike Thomas <mikepthomas@outlook.com>
2026-06-12 14:24:01 +0100

    Disable smooth scroll on route change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html data-scroll-behavior="smooth" lang="en">
       <Head />
       <body>
         <Main />
