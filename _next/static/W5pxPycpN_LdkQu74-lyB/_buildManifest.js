1b26c95a35e917389f40d269727b4486e7b79aeb
Mike Thomas <mikepthomas@outlook.com>
2026-06-28 21:41:42 +0100

    Deploy Site to GitHub Pages

---
+++ _next/static/W5pxPycpN_LdkQu74-lyB/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/00ahkqcsdt0yl.js"
+  ],
+  "/_error": [
+    "static/chunks/26w3-2durp-ey.js"
+  ],
+  "/projects": [
+    "static/chunks/2llebhrbiqmo0.js"
+  ],
+  "/projects/[slug]": [
+    "static/chunks/0havzv1sy4z-5.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/projects",
+    "/projects/[slug]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/2llebhrbiqmo0.js
@@ -0,0 +1,10 @@
+__turbopack_load_page_chunks__("/projects", [
+  "static/chunks/3m54vcxd702yl.js",
+  "static/chunks/2imn2c9h-6ryz.js",
+  "static/chunks/1d0v51le9mmkx.js",
+  "static/chunks/0eowluy177x-9.js",
+  "static/chunks/2aowokzi0zm6x.js",
+  "static/chunks/0mbu5cuoq_yuc.js",
+  "static/chunks/2g75-9mwrd-er.css",
+  "static/chunks/turbopack-0ck-8yd0xfa_5.js"
+])
