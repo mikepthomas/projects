aabde5ffe661c708d779756696d377e3e88386a6
Mike Thomas <mikepthomas@outlook.com>
2026-08-29 23:23:43 +0100

    Display time on hover

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'd LLLL yyyy')}</time>;
+  return <time title={format(date, 'EEEE do LLLL yyyy, h:mm b')} dateTime={dateString}>{format(date, 'd LLLL yyyy')}</time>;
 };
 
 export default DateFormatter;
