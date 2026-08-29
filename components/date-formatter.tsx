18148ddd29d7399f63659a7105eba5900839a927
Mike Thomas <mikepthomas@outlook.com>
2026-08-29 23:25:48 +0100

    Linting

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -6,7 +6,14 @@ type Props = {
 
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString);
-  return <time title={format(date, 'EEEE do LLLL yyyy, h:mm b')} dateTime={dateString}>{format(date, 'd LLLL yyyy')}</time>;
+  return (
+    <time
+      title={format(date, 'EEEE do LLLL yyyy, h:mm b')}
+      dateTime={dateString}
+    >
+      {format(date, 'd LLLL yyyy')}
+    </time>
+  );
 };
 
 export default DateFormatter;
